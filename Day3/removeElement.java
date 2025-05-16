968f8815577bca95d1ba52798e64ceaa29a3ba84
Dhruv Garg <dhruvgarg086@gmail.com>
2025-05-16 21:43:21 +0530

    LeetCode -> 27. Remove Element

---
+++ Day3/removeElement.java
@@ -0,0 +1,17 @@
+// LeetCode -> 27. Remove Element
+
+// https://leetcode.com/problems/remove-element/description/
+
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0 ;
+        for(int i = 0; i < nums.length-1; i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
